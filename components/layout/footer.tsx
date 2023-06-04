8def051ada97c7d68b00e00909eb48508c34a5ed
kjxbyz <kjxbyz@163.com>
2023-06-04 18:10:53 +0800

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
+    <div className="fixed bottom-0 w-full border-gray-200 py-5 text-center">
       <p className="text-gray-500 inline-block mr-2">
         <a
           className="font-medium text-gray-800 underline transition-colors"
