16275bb8cd32eac8851374658afe5c9cd8d451db
kjxbyz <kjxbyz@163.com>
2023-06-04 12:37:12 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+import {useEffect, useState} from 'react';
+import Balancer from "react-wrap-balancer";
+import type { Session } from "next-auth"
+import { Github } from "@/components/shared/icons";
+import { nFormatter } from "@/lib/utils";
+import { useSignInModal } from "@/components/layout/sign-in-modal";
+
+export default async function Home() {
+  // const [session, setSession] = useState<Session | null>(null);
+  // const { SignInModal, setShowSignInModal } = useSignInModal();
+  //
+  // useEffect(() => {
+  //   getSession().then((session) => setSession(session));
+  // }, []);
+  //
+  // if (!session) {
+  //   setShowSignInModal(true);
+  // }
+
+  return (
+    <>
+      {/*<SignInModal />*/}
+      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          <Balancer>Building blocks for your Next project</Balancer>
+        </h1>
+        <p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          <Balancer>
+            An opinionated collection of components, hooks, and utilities for
+            your Next.js project.
+          </Balancer>
+        </p>
+        <div
+          className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
+          style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
+        >
+          <button
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
+          >
+            <Github />
+            <p>
+              <span className="hidden sm:inline-block font-semibold">Join Insider</span>
+            </p>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
