ccb85e2157ae7f32537ec0628623ecbb761bad5c
kjxbyz <kjxbyz@163.com>
2023-06-04 18:41:13 +0800

    update logo

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -30,7 +30,7 @@ export default function NavBar({ session }: { session: Session | null }) {
               height="30"
               className="mr-2 rounded-sm"
             ></Image>
-            <p>FaFa Runner</p>
+            <p>CYF</p>
           </Link>
           <div>
             {session ? (
