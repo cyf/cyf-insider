36a57536c89497abc6c59ebd339eeef723ae7b20
kjxbyz <kjxbyz@163.com>
2023-06-04 18:33:03 +0800

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -26,15 +26,22 @@ export default async function Home() {
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          <Balancer>Building blocks for your Next project</Balancer>
+          <Balancer>CYF内测计划</Balancer>
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
           <Balancer>
-            An opinionated collection of components, hooks, and utilities for
-            your Next.js project.
+            参与内测计划, 您将获得最新的产品体验.
+          </Balancer>
+        </p>
+        <p
+            className="mt-2 animate-fade-up text-center text-red-400 opacity-0 md:text-sm"
+            style={{ animationDelay: "0.35s", animationFillMode: "forwards" }}
+        >
+          <Balancer>
+            我们保证您的个人信息不会被滥用.
           </Balancer>
         </p>
         <div
