c3881f0b19517e49204208e34f4bc5440172288c
kjxbyz <kjxbyz@163.com>
2023-06-04 14:42:34 +0800

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,26 @@
 export default function Footer() {
   return (
     <div className="absolute w-full border-t border-gray-200 bg-white py-5 text-center">
-      <p className="text-gray-500">
-        A free template by{" "}
+      <p className="text-gray-500 inline-block mr-2">
         <a
           className="font-medium text-gray-800 underline transition-colors"
-          href="https://twitter.com/steventey"
+          href="https://chenyifaer.com/fafa-runner/privacy"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Steven Tey
+          Privacy
         </a>
       </p>
+        <p className="text-gray-500 inline-block">
+            <a
+                className="font-medium text-gray-800 underline transition-colors"
+                href="https://chenyifaer.com/fafa-runner/terms-of-use"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Terms of Use
+            </a>
+        </p>
     </div>
   );
 }
