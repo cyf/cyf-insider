a2843f2598c9abb6ad605ff9e29af5da728c6386
kjxbyz <kjxbyz@163.com>
2023-06-06 09:53:13 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,12 @@
 import { withAuth } from "next-auth/middleware";
-import jwt_decode  from 'jwt-decode';
 
 // More on how NextAuth.js middleware works: https://next-auth.js.org/configuration/nextjs#middleware
 export default withAuth({
   callbacks: {
     authorized({ req, token }) {
       console.log('callback token', token, "req", req);
-      const tokenValue = req.cookies.get("next-auth.session-token")?.value;
-      const tokenInSession: any = tokenValue ? jwt_decode(tokenValue) : null;
-
-      console.log('tokenInSession', tokenInSession);
       // `/form` only requires the user to be logged in
-      return !!token || !!tokenInSession;
+      return !!token;
     },
   },
 });
