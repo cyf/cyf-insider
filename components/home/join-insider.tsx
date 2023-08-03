a3fe7644478866358264dbae56f11178f31c32a9
kjxbyz <kjxbyz@163.com>
2023-08-03 15:02:03 +0800

    perf: remove codes

--- components/home/join-insider.tsx
+++ components/home/join-insider.tsx
@@ -5,17 +5,13 @@ import { GrAddCircle } from "react-icons/gr";
 export function JoinInsider() {
   const router = useRouter();
 
-  const joinInsider = async () => {
-    router.push("/form");
-  };
-
   return (
     <div
       className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
       style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
     >
       <button
-        onClick={joinInsider}
+        onClick={() => router.push("/form")}
         className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
       >
         <GrAddCircle />
