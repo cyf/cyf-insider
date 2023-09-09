019f1d5a30bdde81537f77d64622e5fbadaa40e1
kjxbyz <kjxbyz@163.com>
2023-09-09 21:31:22 +0800

    update google icon

--- components/shared/icons/google.tsx
+++ components/shared/icons/google.tsx
@@ -1,47 +1,29 @@
 export default function Google({ className }: { className: string }) {
   return (
-    <svg viewBox="0 0 100 100" className={className}>
-      <linearGradient
-        id="b"
-        x1="55.41"
-        x2="12.11"
-        y1="96.87"
-        y2="21.87"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#1e8e3e" />
-        <stop offset="1" stopColor="#34a853" />
-      </linearGradient>
-      <linearGradient
-        id="c"
-        x1="42.7"
-        x2="86"
-        y1="100"
-        y2="25.13"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#fcc934" />
-        <stop offset="1" stopColor="#fbbc04" />
-      </linearGradient>
-      <linearGradient
-        id="a"
-        x1="6.7"
-        x2="93.29"
-        y1="31.25"
-        y2="31.25"
-        gradientUnits="userSpaceOnUse"
-      >
-        <stop offset="0" stopColor="#d93025" />
-        <stop offset="1" stopColor="#ea4335" />
-      </linearGradient>
-      <path fill="url(#a)" d="M93.29 25a50 50 90 0 0-86.6 0l3 54z" />
-      <path fill="url(#b)" d="M28.35 62.5 6.7 25A50 50 90 0 0 50 100l49-50z" />
-      <path fill="url(#c)" d="M71.65 62.5 50 100a50 50 90 0 0 43.29-75H50z" />
-      <path fill="#fff" d="M50 75a25 25 90 1 0 0-50 25 25 90 0 0 0 50z" />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      className={className}
+    >
       <path
-        fill="#1a73e8"
-        d="M50 69.8a19.8 19.8 90 1 0 0-39.6 19.8 19.8 90 0 0 0 39.6z"
-      />{" "}
+        d="M5.23762 10.0689C5.23762 9.49614 5.33413 8.94728 5.50489 8.43182L2.50654 6.16211C1.90322 7.37613 1.59025 8.71374 1.5923 10.0694C1.5923 11.4731 1.92108 12.7978 2.50442 13.9735L5.50224 11.7001C5.32724 11.1742 5.23789 10.6236 5.23762 10.0694"
+        fill="#FBBC05"
+      />
+      <path
+        d="M10.5089 4.8582C11.7037 4.85571 12.8627 5.26597 13.7899 6.01957L16.3825 3.45501C14.8027 2.09213 12.7775 1.25 10.5089 1.25C6.98657 1.25 3.95958 3.24607 2.51025 6.16222L5.51073 8.43193C6.20171 6.35313 8.17127 4.85767 10.5099 4.85767"
+        fill="#EA4335"
+      />
+      <path
+        d="M10.5441 15.1653C8.19323 15.1653 6.2136 13.6799 5.5189 11.6133L2.50464 13.8692C3.96139 16.7673 7.00375 18.7507 10.5446 18.7507C12.7284 18.7507 14.8136 17.9913 16.3796 16.5663L13.518 14.399C12.7114 14.8975 11.6943 15.1658 10.5435 15.1658"
+        fill="#34A853"
+      />
+      <path
+        d="M19.0933 9.98615C19.0933 9.46857 19.0117 8.91016 18.8897 8.39258H10.5432V11.7786H15.3473C15.1076 12.9336 14.4542 13.8208 13.5182 14.3983L16.3792 16.5657C18.0237 15.0707 19.0933 12.8429 19.0933 9.98615Z"
+        fill="#4285F4"
+      />
     </svg>
   );
 }
