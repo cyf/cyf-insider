b74cd374e482028bf7f1e56dbff41b907e5ecf46
kjxbyz <kjxbyz@163.com>
2023-10-02 19:07:28 +0800

    perf: Improve user experience

--- components/layout/sign-in-modal.tsx
+++ components/layout/sign-in-modal.tsx
@@ -19,8 +19,15 @@ const SignInModal = ({
   showSignInModal: boolean;
   setShowSignInModal: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const [checked, setChecked] = useState(false);
+  const [showRed, setRed] = useState(false);
   const [googleClicked, setGoogleClicked] = useState(false);
   const [githubClicked, setGitHubClicked] = useState(false);
+
+  const onCheckboxClick = (e: any) => {
+    e.target.checked && setRed(false);
+    setChecked(e.target.checked)
+  }
 
   return (
     <Modal showModal={showSignInModal} setShowModal={setShowSignInModal}>
@@ -50,6 +57,10 @@ const SignInModal = ({
                 : "border border-gray-200 bg-white text-black hover:bg-gray-50 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-700"
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
+              if (!checked) {
+                setRed(true);
+                return;
+              }
               setGoogleClicked(true);
               signIn("google");
             }}
@@ -71,6 +82,10 @@ const SignInModal = ({
                 : "border border-gray-200 bg-white text-black hover:bg-gray-50 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-700"
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
+              if (!checked) {
+                setRed(true);
+                return;
+              }
               setGitHubClicked(true);
               signIn("github");
             }}
@@ -85,8 +100,9 @@ const SignInModal = ({
             )}
           </button>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-2 text-center dark:border-gray-700 dark:bg-gray-900 sm:px-16">
-          <p className="text-sm text-gray-500">
+        <div className="flex flex-row items-start justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-2 text-center dark:border-gray-700 dark:bg-gray-900 sm:px-16">
+          <input checked={checked} type="checkbox" onClick={onCheckboxClick} className={`w-4 h-4 mt-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 ${showRed ? "border-red-400 border-2" : ""}`} />
+          <p className="text-sm text-gray-500 mt-3">
             I have carefully read and agreed to <Link className="text-blue-500" href="/legal/privacy">Privacy Policy</Link>
             {" "}and <Link className="text-blue-500" href="/legal/terms-of-use">Terms and Conditions</Link>
           </p>
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import {MouseEvent, useState} from "react";
 import { signIn } from "next-auth/react";
 import { Github, Google, LoadingDots } from "@/components/shared/icons";
 import { useRouter } from "next/router";
@@ -8,8 +8,15 @@ import { useRouter } from "next/router";
 export default function SignIn() {
   const router = useRouter();
   const { callbackUrl } = router.query as { callbackUrl?: string };
+  const [checked, setChecked] = useState(false);
+  const [showRed, setRed] = useState(false);
   const [googleClicked, setGoogleClicked] = useState(false);
   const [githubClicked, setGitHubClicked] = useState(false);
+
+  const onCheckboxClick = (e: any) => {
+    e.target.checked && setRed(false);
+    setChecked(e.target.checked)
+  }
 
   return (
     <div className="flex w-screen justify-center">
@@ -38,6 +45,10 @@ export default function SignIn() {
                 : "border border-gray-200 bg-white text-black hover:bg-gray-50 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-700"
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
+              if (!checked) {
+                setRed(true);
+                return;
+              }
               setGoogleClicked(true);
               signIn("google", {
                 ...(callbackUrl ? { callbackUrl } : {}),
@@ -63,6 +74,10 @@ export default function SignIn() {
                 : "border border-gray-200 bg-white text-black hover:bg-gray-50 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-700"
             } flex h-10 w-full items-center justify-center space-x-3 rounded-md border text-sm shadow-sm transition-all duration-75 focus:outline-none`}
             onClick={() => {
+              if (!checked) {
+                setRed(true);
+                return;
+              }
               setGitHubClicked(true);
               signIn("github", {
                 ...(callbackUrl ? { callbackUrl } : {}),
@@ -81,8 +96,9 @@ export default function SignIn() {
             )}
           </button>
         </div>
-        <div className="flex flex-col items-center justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-2 text-center dark:border-gray-700 dark:bg-gray-900 sm:px-16">
-          <p className="text-sm text-gray-500">
+        <div className="flex flex-row items-start justify-center space-y-3 border-b border-gray-200 bg-white px-4 py-2 text-center dark:border-gray-700 dark:bg-gray-900 sm:px-16">
+          <input checked={checked} type="checkbox" onClick={onCheckboxClick} className={`w-4 h-4 mt-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 ${showRed ? "border-red-400 border-2" : ""}`} />
+          <p className="text-sm text-gray-500 mt-3">
             I have carefully read and agreed to <Link className="text-blue-500" href="/legal/privacy">Privacy Policy</Link>
             {" "}and <Link className="text-blue-500" href="/legal/terms-of-use">Terms and Conditions</Link>
           </p>
